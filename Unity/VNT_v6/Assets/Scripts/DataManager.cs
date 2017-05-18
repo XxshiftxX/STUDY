14ec1ec7ec5aab4befdba54dc19d75b21bfa2513
shift <qjfrntop12@naver.com>
2017-05-19 08:25:43 +0900

    VNT 개발 1일차

---
+++ Unity/VNT_v6/Assets/Scripts/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 게임에서 쓰이는 데이터 관리
+public class DataManager : MonoBehaviour {
+
+	public int Playerdata_pageIndex; // 플레이어가 실행했던 페이지(스크립트 파일)
+	public int Playerdata_lineIndex; // 플레이어가 실행했던 라인
+
+	public ArrayList Gamedata_scriptLine = new ArrayList(); // 게임의 스크립트의 라인이 들어갈 변수
+
+	public TextAsset[] Assetdata_scripts; // 스크립트 파일들이 들어갈 변수
+
+	// 특정 스크립트를 로드하여 Gamedata_scriptLine에 넣음 (script : 로드할 스크립트)
+	public void scriptLoad(string script){
+		Gamedata_scriptLine.Clear ();
+		foreach (string s in Assetdata_scripts[0].text.Split('\n'))
+		{
+			Gamedata_scriptLine.Add (s);
+		}
+	}
+
+	void Start()
+	{
+		scriptLoad("a");
+	}
+}
---
+++ Unity/VNT_v6/Assets/Scripts/LineManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineManager : MonoBehaviour {
+
+	public Text Object_CharText; // 캐릭터 이름을 표기하는 Text UI
+	public Text Object_LineText; // 대사를 표기하는 Text UI
+
+	// Use this for initialization
+	void Start () {
+		DataManager dm = new DataManager();
+		dm.scriptLoad("test");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
