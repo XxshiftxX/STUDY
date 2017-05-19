60c05050b0890d4e812ba73eff3a91dbf2b60841
shift <qjfrntop12@naver.com>
2017-05-19 23:48:36 +0900

    VNT 개발 2일차

--- Unity/VNT_v6/Assets/Scripts/DataManager.cs
+++ Unity/VNT_v6/Assets/Scripts/DataManager.cs
@@ -8,21 +8,17 @@ public class DataManager : MonoBehaviour {
 	public int Playerdata_pageIndex; // 플레이어가 실행했던 페이지(스크립트 파일)
 	public int Playerdata_lineIndex; // 플레이어가 실행했던 라인
 
-	public ArrayList Gamedata_scriptLine = new ArrayList(); // 게임의 스크립트의 라인이 들어갈 변수
+	public string[] Gamedata_scriptLine; // 게임의 스크립트의 라인이 들어갈 변수
 
 	public TextAsset[] Assetdata_scripts; // 스크립트 파일들이 들어갈 변수
 
 	// 특정 스크립트를 로드하여 Gamedata_scriptLine에 넣음 (script : 로드할 스크립트)
 	public void scriptLoad(string script){
-		Gamedata_scriptLine.Clear ();
-		foreach (string s in Assetdata_scripts[0].text.Split('\n'))
-		{
-			Gamedata_scriptLine.Add (s);
-		}
+		Gamedata_scriptLine = script.Split('\n');
 	}
 
 	void Start()
 	{
-		scriptLoad("a");
+		scriptLoad(Assetdata_scripts[0].text);
 	}
 }
--- Unity/VNT_v6/Assets/Scripts/LineManager.cs
+++ Unity/VNT_v6/Assets/Scripts/LineManager.cs
@@ -7,15 +7,36 @@ public class LineManager : MonoBehaviour {
 
 	public Text Object_CharText; // 캐릭터 이름을 표기하는 Text UI
 	public Text Object_LineText; // 대사를 표기하는 Text UI
+	public DataManager Class_DataManager; // DataManager 클래스
 
-	// Use this for initialization
-	void Start () {
-		DataManager dm = new DataManager();
-		dm.scriptLoad("test");
+	public int line_Index; // 플레이어의 현재 라인
+	int line_Status; // 라인 진행의 상태 (대기, 진행중, 다음 진행 대기)
+	int line_Process; // 문장을 자를 때 쓸것
+
+	void Start()
+	{
+		lineIndex = Playerdata_lineIndex;
 	}
 
-	// Update is called once per frame
-	void Update () {
-
+	public static void screenTouch() // 스크린 터치 시의 행동;
+	{
+		switch(Class_DataManager.Gamedata_scriptLine.Split('|')[0])
+		{
+			case "com":
+				break;
+			case "l":
+				switch(lineStatus)
+				{
+					case 0:
+						line_Process = 0;
+						break;
+					case 1:
+						line_Process = Class_DataManager.Gamedata_scriptLine[line_Index].Split('|')[2];
+						break;
+					case 2:
+						break;
+				}
+				break;
+		}
 	}
 }
